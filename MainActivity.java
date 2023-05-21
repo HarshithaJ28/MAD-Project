0ba8a9bba878afe594668827137392996ed661e7
HarshithaJ28 <92093743+HarshithaJ28@users.noreply.github.com>
2023-05-21 16:12:49 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.onroadserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button switchToSecondActivity;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchToSecondActivity = findViewById(R.id.register);
+        switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this,MainActivityRegister.class);
+        startActivity(switchActivityIntent);
+    }
+}
